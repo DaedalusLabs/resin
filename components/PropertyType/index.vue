e47cc8fbbdb932992a6bdf37f8e4312f874d5878
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-07-21 17:13:04 +0200

    feat: added choose property type page

---
+++ components/PropertyType/index.vue
@@ -0,0 +1,56 @@
+<template>
+   <div>
+      <a
+         href="#"
+         :class="{ 'border-resin-500': isSelected }"
+         class="block h-full max-w-sm rounded-2xl border-4 border-gray-200 bg-white p-6 pr-4 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+         @click="isSelected = !isSelected"
+      >
+         <div class="flex justify-between">
+            <h5
+               class="mb-2 w-3/4 text-lg font-bold leading-tight tracking-tight text-gray-900"
+            >
+               {{ title }}
+            </h5>
+            <div
+               class="flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-200"
+               :class="{ 'bg-resin-500': isSelected }"
+            >
+               <svg
+                  v-if="isSelected"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-4 w-4 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+               >
+                  <path
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="3"
+                     d="M5 13l4 4L19 7"
+                  />
+               </svg>
+            </div>
+         </div>
+         <p class="font-normal text-gray-400">
+            {{ description }}
+         </p>
+      </a>
+   </div>
+</template>
+
+<script setup>
+defineProps({
+   title: {
+      type: String,
+      required: true,
+   },
+   description: {
+      type: String,
+      required: true,
+   },
+});
+
+const isSelected = ref(false);
+</script>
---
+++ pages/choosepropertyType.vue
@@ -0,0 +1,72 @@
+<template>
+   <section class="flex flex-col items-center justify-between px-12 pt-20">
+      <h1
+         class="text-center font-inter text-2xl font-extrabold leading-tight text-white"
+      >
+         {{ $t("propertyType") }}
+      </h1>
+      <div class="mt-10 grid grid-cols-1 gap-6 md:grid-cols-3">
+         <PropertyType
+            v-for="propertyType in propertyTypes"
+            :key="propertyType.id"
+            :title="$t(propertyType.title)"
+            :description="$t(propertyType.description)"
+            class="w-full flex-grow"
+            @click="addPropertyType(propertyType.id)"
+         />
+      </div>
+      <div class="flex flex-col items-center justify-center gap-2">
+         <FlowbiteButton
+            v-if="selectedPropertyTypes.length > 0"
+            class="mt-10"
+            :text="$t('chooseLocation')"
+         />
+         <FlowbiteDisabledButton
+            v-else
+            class="mt-10"
+            :text="$t('chooseLocation')"
+         />
+         <NuxtLink
+            v-if="selectedPropertyTypes.length < 1"
+            to="#"
+            class="mt-4 rounded-lg px-5 py-2.5 font-inter text-sm font-medium text-pirate-400 hover:bg-white hover:text-pirate-700"
+         >
+            {{ $t("skip") }}
+         </NuxtLink>
+      </div>
+   </section>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const selectedPropertyTypes = ref([]);
+const propertyTypes = [
+   {
+      id: 1,
+      title: "propertyTypes.shortToMediumTermStay.title",
+      description: "propertyTypes.shortToMediumTermStay.description",
+   },
+   {
+      id: 2,
+      title: "propertyTypes.longerTermResidentialHouse.title",
+      description: "propertyTypes.longerTermResidentialHouse.description",
+   },
+   {
+      id: 3,
+      title: "propertyTypes.commercialProperty.title",
+      description: "propertyTypes.commercialProperty.description",
+   },
+];
+
+const addPropertyType = (propertyTypeId) => {
+   if (selectedPropertyTypes.value.includes(propertyTypeId)) {
+      selectedPropertyTypes.value = selectedPropertyTypes.value.filter(
+         (id) => id !== propertyTypeId,
+      );
+   } else {
+      selectedPropertyTypes.value.push(propertyTypeId);
+   }
+   console.log(selectedPropertyTypes.value);
+};
+</script>
