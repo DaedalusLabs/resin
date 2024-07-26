f030d8d1e8b8a32a6b8711272e23c287f916e0e3
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-07-26 13:08:32 +0200

    feat: added remove option for desktop users

--- components/Favorites/Card.vue
+++ components/Favorites/Card.vue
@@ -1,7 +1,7 @@
 <template>
    <div
       v-touch:swipe.left="swipeHandler"
-      class="flex items-center rounded-lg border-2 border-pirate-100 bg-white p-4 shadow-md transition-transform duration-300 ease-in-out"
+      class="group relative flex items-center rounded-lg border-2 border-pirate-100 bg-white p-4 shadow-md transition-transform duration-300 ease-in-out"
       :class="{ '-translate-x-96 transform': isRemoving }"
    >
       <NuxtImg
@@ -16,6 +16,27 @@
             {{ price }} SRD per month
          </p>
       </div>
+      <!-- Trash icon -->
+      <div
+         class="absolute right-4 top-1/2 flex h-8 w-8 -translate-y-1/2 cursor-pointer items-center justify-center rounded-full bg-red-600 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+         @click="handleRemove"
+      >
+         <svg
+            class="h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+         >
+            <path
+               stroke-linecap="round"
+               stroke-linejoin="round"
+               stroke-width="2"
+               d="M6 18L18 6M6 6l12 12"
+            />
+         </svg>
+      </div>
    </div>
 </template>
 
@@ -29,6 +50,10 @@ const swipeHandler = () => {
    setTimeout(() => {
       emit("remove");
    }, 300);
+};
+
+const handleRemove = () => {
+   emit("remove");
 };
 
 defineProps({
