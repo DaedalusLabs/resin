9601506cd38b9b981710936a07000afa521f8313
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-08-20 16:54:32 +0200

    feat: added toast message to map

---
+++ components/Flowbite/Toast.vue
@@ -0,0 +1,40 @@
+<template>
+   <div
+      v-if="showToast"
+      class="force-top absolute inset-0 flex items-center justify-center"
+   >
+      <div
+         id="toast-message-cta"
+         class="w-full max-w-xs rounded-lg bg-white p-4 text-gray-500 shadow dark:bg-gray-800 dark:text-gray-400"
+         role="alert"
+      >
+         <div class="flex">
+            <NuxtImg
+               class="h-8 w-8 rounded-full"
+               src="/images/avatar.png"
+               alt="avatar"
+            />
+            <div class="ms-3 text-sm font-normal">
+               <span
+                  class="mb-1 text-sm font-semibold text-gray-900 dark:text-white"
+                  >Let's find a property!</span
+               >
+               <div class="mb-2 text-sm font-normal">
+                  RESIN offers appartments, single family homes, commercial real
+                  estate and more around LatAm.
+               </div>
+               <a
+                  href="#"
+                  class="inline-flex rounded-lg bg-resin-500 px-2.5 py-1.5 text-center text-xs font-medium text-white hover:bg-resin-600 focus:outline-none focus:ring-4 focus:ring-resin-300 dark:bg-resin-500 dark:hover:bg-resin-600 dark:focus:ring-resin-800"
+                  @click="showToast = false"
+                  >Continue</a
+               >
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup>
+const showToast = ref(true);
+</script>
--- pages/map.vue
+++ pages/map.vue
@@ -1,12 +1,20 @@
 <template>
-   <section class="flex h-full flex-col items-center justify-between">
+   <section class="relative flex h-full flex-col items-center justify-between">
       <TopBar
          class="force-top absolute left-0 right-0 top-0 px-5 pt-10"
          locations="locations"
       />
-      <ClientOnly fallback-tag="span" fallback="Loading map...">
+      <FlowbiteToast />
+      <ClientOnly fallback-tag="span">
          <MapLeaflet />
+         <template #fallback>
+            <!-- this will be rendered on server side -->
+            <div
+               class="absolute inset-0 flex h-full w-full items-center justify-center bg-gray-100 dark:bg-gray-800"
+            />
+         </template>
       </ClientOnly>
+      <!-- div while map loads -->
    </section>
 </template>
 
