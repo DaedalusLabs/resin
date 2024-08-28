2f3586fdeda51a1e3b1238fc9371cf06741ae0d9
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-08-29 01:50:05 +0200

    style: added bit more margin for small screens

--- pages/choose-property-type.vue
+++ pages/choose-property-type.vue
@@ -28,7 +28,7 @@
          <NuxtLink
             v-if="selectedPropertyTypes.length < 1"
             :to="localePath('map')"
-            class="mt-4 rounded-lg px-5 py-2.5 text-sm font-medium text-pirate-400 hover:bg-white hover:text-pirate-700"
+            class="mt-4 rounded-lg px-5 py-2.5 mb-10 text-sm font-medium text-pirate-400 hover:bg-white hover:text-pirate-700"
          >
             {{ $t("skip") }}
          </NuxtLink>
