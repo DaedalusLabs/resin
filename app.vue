fa88b8cfe2130b6999725f52e2dab3094d896dfb
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-09-16 14:42:26 +0200

    feat: favorites now redirect to details page when clicked

--- app.vue
+++ app.vue
@@ -267,4 +267,53 @@ async function getProperties() {
 }
 
 propertiesStore.properties = await getProperties();
+propertiesStore.filteredProperties = propertiesStore.properties;
+console.log(propertiesStore.properties);
 </script>
+
+<style>
+::-moz-selection {
+   /* Code for Firefox */
+   color: white;
+   background: #f07e19;
+}
+
+::selection {
+   color: white;
+   background: #f07e19;
+}
+
+html {
+   scroll-behavior: smooth;
+}
+
+/* font Inter for all text */
+* {
+   font-family: "Inter", sans-serif;
+   font-optical-sizing: auto;
+   font-style: normal;
+   font-variation-settings: "slnt" 0;
+}
+
+/* clears the ‘X’ from Internet Explorer */
+input[type="search"]::-ms-clear {
+   display: none;
+   width: 0;
+   height: 0;
+}
+input[type="search"]::-ms-reveal {
+   display: none;
+   width: 0;
+   height: 0;
+} /* clears the ‘X’ from Chrome */
+input[type="search"]::-webkit-search-decoration,
+input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-results-button,
+input[type="search"]::-webkit-search-results-decoration {
+   display: none;
+}
+
+.z-top {
+   z-index: 1000;
+}
+</style>
